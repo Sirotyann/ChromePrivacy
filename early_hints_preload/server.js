0179bb13e769dc75530e60ce5092fbca84be0558
Cheng Li <Cheng.Li@mindgeek.com>
2022-09-20 02:00:02 +0800

    remove unused codes

--- early_hints_preload/server.js
+++ early_hints_preload/server.js
@@ -1,24 +1,8 @@
 const express = require('express')
 const path = require('path');
 const app = express()
-// const earlyHints = require('early-hints')
-// app.use(earlyHints([
-//   { path: '/style.css', rel: 'preload' },
-//   { path: '/main.js', rel: 'preload', as: 'script' }
-// ]))
-
-const CRLF = '\r\n'
-const PRELOAD = 'preload'
-
-// app.use(express.static('public', {
-//     setHeaders: function (res, path, stat) {
-//         res.setHeader('supports-loading-mode', 'fenced-frame');
-//         res.set('Origin-Trial', 'Aw0/VBaSpR35KUWf94+YZ7ki6LS06lQzGx33SGIyNe5xdvipD71lVfO/ot9xIhZn9+ntQsN6GlPR2Ys98pnJCAoAAABteyJvcmlnaW4iOiJodHRwczovLzEyNy4wLjAuMTo5NDQzIiwiZmVhdHVyZSI6IlByaXZhY3lTYW5kYm94QWRzQVBJcyIsImV4cGlyeSI6MTY2MTI5OTE5OSwiaXNUaGlyZFBhcnR5Ijp0cnVlfQ==');
-//     }
-// }));
 
 app.use((req, res, next) => {
-    console.log(req.path)
     if (req.path !== '/' && req.path !== 'index.html') {
         return next();
     }
@@ -35,35 +19,7 @@ app.use((req, res, next) => {
 });
 
 app.get('/', function (req, res) {
-    console.log(" get index")
-    res.setHeader('Content-Type', 'text/html');
-    res.set('Origin-Trial', 'Aw0/VBaSpR35KUWf94+YZ7ki6LS06lQzGx33SGIyNe5xdvipD71lVfO/ot9xIhZn9+ntQsN6GlPR2Ys98pnJCAoAAABteyJvcmlnaW4iOiJodHRwczovLzEyNy4wLjAuMTo5NDQzIiwiZmVhdHVyZSI6IlByaXZhY3lTYW5kYm94QWRzQVBJcyIsImV4cGlyeSI6MTY2MTI5OTE5OSwiaXNUaGlyZFBhcnR5Ijp0cnVlfQ==');
     res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
-app.use(express.static(path.join(__dirname, 'public'), {
-    setHeaders: function (res, path, stat) {
-        res.set('Origin-Trial', 'Aw0/VBaSpR35KUWf94+YZ7ki6LS06lQzGx33SGIyNe5xdvipD71lVfO/ot9xIhZn9+ntQsN6GlPR2Ys98pnJCAoAAABteyJvcmlnaW4iOiJodHRwczovLzEyNy4wLjAuMTo5NDQzIiwiZmVhdHVyZSI6IlByaXZhY3lTYW5kYm94QWRzQVBJcyIsImV4cGlyeSI6MTY2MTI5OTE5OSwiaXNUaGlyZFBhcnR5Ijp0cnVlfQ==');
-    }
-}));
-
-app.listen(8080)
-
-
-
-// const http = require('http')
-// const fs = require('fs')
-// const earlyHints = require('early-hints')
-// console.log(__dirname)
-// const server = http.createServer((req, res) => {
-//     console.log(req.url)
-//     if (req.url === '/') {
-//         earlyHints(['/style.css'])(req, res)
-//     } else if (req.url === '/style.css') {
-//         res.setHeader('Content-Type', 'text/css')
-//         return fs.createReadStream(__dirname + `/public/${req.url}`).pipe(res)
-//     }
-
-//     res.setHeader('Content-Type', 'text/html')
-//     fs.createReadStream(__dirname + '/public/index.html').pipe(res)
-// }).listen(9000)
+app.listen(3000)
--- early_hints_preload/test.js
+++
@@ -1,5 +0,0 @@
-const http = require('http')
-
-http.get('http://localhost:8080/', (res) => {
-    console.log(`status code : ${res.statusCode}`)
-})
